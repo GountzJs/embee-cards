978c83d7c3c7b75ff76ca7abadfe5981250725a4
GountzJs <tomasjsdev@gmail.com>
2025-03-05 08:31:01 -0300

    feat: cardmove create component

---
+++ lib/components/card-move/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+interface CardMoveProps {
+  children: React.ReactNode;
+  maxTilt?: number;
+  perspective?: number;
+}
+
+export function CardMove({
+  children,
+  maxTilt = 15,
+  perspective = 1000,
+}: CardMoveProps) {
+  const [tilt, setTilt] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const xPercent = (x / rect.width - 0.5) * 2;
+    const yPercent = (y / rect.height - 0.5) * 2;
+
+    const tiltX = yPercent * maxTilt;
+    const tiltY = xPercent * -maxTilt;
+
+    setTilt({ x: tiltX, y: tiltY });
+  };
+
+  const handleMouseLeave = () => {
+    setTilt({ x: 0, y: 0 });
+  };
+
+  return (
+    <div
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      className="w-[300px] h-[450px] relative transition-transform duration-200 ease-out"
+      style={{
+        transform: `perspective(${perspective}px) rotateX(${tilt.x}deg) rotateY(${tilt.y}deg)`,
+        transformStyle: "preserve-3d",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
--- lib/components/index.ts
+++ lib/components/index.ts
@@ -3,6 +3,7 @@ export * from './card-cover';
 export * from './card-decorator';
 export * from './card-description';
 export * from './card-flipped';
+export * from './card-move';
 export * from './card-number';
 export * from './card-stars';
 export * from './card-title';
